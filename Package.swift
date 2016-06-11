809f945f5cfb7c6a2593efbe32706c56a75ca1a1
Andrew Thompson <mrwerdo331@me.com>
2016-06-11 16:37:47 +1000

    Flatted Sources Layout

--- Package.swift
+++ Package.swift
@@ -1,12 +1,8 @@
 import PackageDescription
 
 let package = Package(
-	name: "TIFF",
+	name: "CLibTIFF",
 	targets: [ // ]
-        Target(name: "CLibTIFF"),
-		Target(name: "LibTIFF", dependencies: ["CLibTIFF"])
-	],
-	dependencies: [
-		.Package(url: "https://github.com/mrwerdo/Geometry", majorVersion: 1)
+        Target(name: "CLibTIFF")
 	]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import LibTIFFTestSuite
-
-XCTMain([
-	 testCase(LibTIFFTests.allTests),
-])
